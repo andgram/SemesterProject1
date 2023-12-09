57d428b93e408eae26a22355f67d5fbfd4e8d945
andgram <andgra00184@stud.noroff.no>
2023-12-09 22:48:54 +0100

    Alt text adjust

---
+++ entrance-anim.js
@@ -0,0 +1,25 @@
+ 
+ const sectionOne = document.querySelector('.section1');
+ const sections = document.querySelectorAll("section");
+
+ const options = {
+   root: null, // it is the viewport
+   threshold: 0,
+   rootMargin: "-150px"
+ };
+
+ const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+
+    if (!entry.isIntersecting) {
+      return;
+    }
+
+    entry.target.classList.toggle("inverse");
+    observer.unobserve(entry.target);
+  });
+ }, options);
+
+  sections.forEach(section => {
+  observer.observe(section);
+ });
